c3e65d93f589f950d158810d9c35737b19237e9c
starshynova <l.starshynova@gmail.com>
2024-11-06 15:54:09 +0100

    prep-ex1

--- Week2/prep-exercises/1-catwalk-async-await/index.js
+++ Week2/prep-exercises/1-catwalk-async-await/index.js
@@ -7,24 +7,46 @@ const DANCING_CAT_URL =
   'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
 
 function walk(img, startPos, stopPos) {
+  img.style.left = startPos + 'px';
+  let currentLeftPos = startPos;
   return new Promise((resolve) => {
-    // Copy over the implementation from last week
+    function catMoving () {
+      currentLeftPos = currentLeftPos + STEP_SIZE_PX;
+      img.style.left = currentLeftPos +'px';
+   
+    if (currentLeftPos <= stopPos) {
+      setTimeout(catMoving, STEP_INTERVAL_MS)
+    } else {resolve();}
+  }
+  catMoving();
   });
 }
 
 function dance(img) {
   return new Promise((resolve) => {
-    // Copy over the implementation from last week
+    img.src = DANCING_CAT_URL;
+    setTimeout(() => {
+      img.src =
+        'http://www.anniemation.com/clip_art/images/cat-walk.gif';
+        resolve();
+    }, DANCE_TIME_MS);
   });
 }
 
-async function catWalk() {
+function catWalk() {
   const img = document.querySelector('img');
   const startPos = -img.width;
   const centerPos = (window.innerWidth - img.width) / 2;
   const stopPos = window.innerWidth;
 
-  // Use async/await syntax to loop the walk and dance functions
+  async function walkingCat() {
+    await walk(img, startPos, centerPos);
+    await dance(img);
+    await walk(img, centerPos,stopPos);
+    walkingCat();
+  }
+
+  walkingCat();
 }
 
 window.addEventListener('load', catWalk);
